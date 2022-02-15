33c5a0359de48326fec6a3c1601e4fbb4145e9f8
Soocone <wltn3185@naver.com>
2022-02-16 00:43:49 +0900

    0216 00:43 homecontroller mypage부분 매핑 이관 KJS

--- src/main/java/com/kosmo/freepproject/MypageController.java
+++ src/main/java/com/kosmo/freepproject/MypageController.java
@@ -1,12 +1,19 @@
 package com.kosmo.freepproject;
 
+import java.security.Principal;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public class MypageController {
 	
 	@RequestMapping("/mypage/myMain.do")
-	public String myMain() {		
-		return "mypage/myMain";}
+	public String myMain(Principal principal, Model model) {		
+		String user_id = principal.getName();
+		model.addAttribute("user_id", user_id);
+		
+		return "mypage/myMain";
+	}
 	@RequestMapping("/mypage/myOrder.do")
 	public String myOrder() {return "mypage/myOrder";}
 	@RequestMapping("/mypage/myCoupon.do")
